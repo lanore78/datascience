2c2a008ed6a33cffe804171acec757098314d826
lanore <tlaqlsrn89#>
2017-11-06 22:45:11 +0900

    CSV Load

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def main():
+    csv_test = pd.read_csv('./dds_ch2_nyt/nyt1.csv')
+    print(csv_test)
+
+
+if __name__ == "__main__":
+    main()
